c4d89ee3e9c992d355961d1bbe6eb1f5d8ed6d3d
Reahana <reahana233@gmail.com>
2023-03-19 12:59:08 +0600

    error handel

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,19 +2,32 @@ document.getElementById('withdawBtn').addEventListener('click',function(){
     //get withdraw amount from the input field
     const withdrawAmountField = document.getElementById('withdrawAmount')
 const newWithdrawAmount = parseFloat(withdrawAmountField.value);
-
+withdrawAmountField.value ='' ;
+if(isNaN(newWithdrawAmount)){
+    alert('please provide a valid number ')
+    return
+}
 //get the total deposit
 const withdrawTotalAmount= document.getElementById('withdrawTotal')
 const previousWithdrawTotal = parseFloat(withdrawTotalAmount.innerText) ;
 
-//update the deposite amount
-const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-withdrawTotalAmount.innerText = currentWithdrawTotal;
+
 
 
 // get balance 
 const balanceTotalAmount= document.getElementById('balanceTotal')
 const previousBalanceTotal = parseFloat(balanceTotalAmount.innerText) ;
+withdrawAmountField.value ='' ;
+
+if(newWithdrawAmount > previousBalanceTotal){
+    alert('you dont have enough money')
+    return
+}
+
+
+//update the deposite amount
+const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+withdrawTotalAmount.innerText = currentWithdrawTotal;
 
 // update final balance
 const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
